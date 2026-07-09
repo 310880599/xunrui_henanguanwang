1f84233fbd58b89629514c690cb373e5a1f46b61
思维导图 <310880599@qq.com>
2026-07-09 16:16:06 +0800

    河南官网新闻同步的插件的作者名字被纠正为河南豫工

--- dayrui/App/ContentSyncReceiver/Config/App.php
+++ dayrui/App/ContentSyncReceiver/Config/App.php
@@ -3,7 +3,7 @@
 return [
 
     'type' => 'app',
-    'author' => '禹工机械',
+    'author' => '豫工机械',
     'name' => '新闻同步接收',
     'icon' => 'fa fa-rss',
     'uri' => 'contentsyncreceiver/api/config',
